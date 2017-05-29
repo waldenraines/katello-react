a61fd9e885e1861fdc6477ed890d091cb2c78e89
Walden Raines <walden@waldenraines.com>
2017-05-29 16:51:41 +0300

    Adding patternfly css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import 'patternfly/dist/css/patternfly.css'
+import 'patternfly/dist/css/patternfly-additions.css'
+
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
